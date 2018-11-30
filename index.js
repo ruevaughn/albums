879888d63aa47e6b431804171a988a9ee6b0fd31
Chase Jensen <soccerchase@gmail.com>
2018-11-30 15:48:29 -0700

    Import header component and display it as a child component using the App Parent component

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header'
 
 const App = () => (
-  <Text>Some Text</Text>
+  <Header />
 );
 
 AppRegistry.registerComponent('udemyalbums', () => App);
