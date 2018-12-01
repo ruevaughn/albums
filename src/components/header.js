25966aa9578b08e56abea9e45f08d3d70f597d7a
Chase Jensen <soccerchase@gmail.com>
2018-11-30 20:52:32 -0700

    Add More styling and fix mistake

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@
     const { textStyle, viewStyle } = styles;
 
     return(
-      <View style="viewStyle">
+      <View style={viewStyle}>
         <Text style={textStyle}>Albums</Text>
       </View>
     );
@@ -13,7 +13,16 @@
 
   const styles = {
     viewStyle: {
-      backgroundColor: '#F8F8F8'
+      backgroundColor: '#F8F8F8',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 60,
+      paddingTop: 25,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      elevation: 2,
+      position: 'relative'
     },
     textStyle: {
       fontSize: 20
