75ef59fece2a9a8a3c0e699213b0850b00811382
Chase Jensen <soccerchase@gmail.com>
2018-12-01 08:40:53 -0700

    Basic http request to api using axios

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
   componentWillMount() {
-    console.log('componentWillMount');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response.data))
   }
   render() {
     return (
