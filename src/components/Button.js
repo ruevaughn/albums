22d05b313e3d7c0146998c1d69d062857d3945fc
Chase Jensen <soccerchase@gmail.com>
2018-12-08 12:04:22 -0700

    Add stylin to button tag

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,38 @@ import { Text, TouchableOpacity } from 'react-native'
 // TouchableOpacity - Treated like it's a View tag
 
 const Button = () => {
+  const { buttonStyle, textStyle } = styles;
   return (
-    <TouchableOpacity>
-      <Text>Click me!!!</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>Click me!!!</Text>
     </TouchableOpacity>
   );
 }
+
+const styles = {
+  // flex: 1 - expand to fill as much content as parent element allows
+  // alignSelf - is a property 'type' of flexbox, says to stretch to fill
+  //             the limits of the container.
+  // alignSelf - 'center' - center the text inside button
+
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+};
 
 export default Button;
