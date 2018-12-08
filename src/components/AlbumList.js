c6c2194a99b05538a2c92deac0ff6b804fb979f0
Chase Jensen <soccerchase@gmail.com>
2018-12-07 21:39:27 -0700

    Add ScrollView rather than View component and add a flex of 1

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import AlbumDetail from './AlbumDetail'
 import axios from 'axios';
 
@@ -21,9 +21,9 @@ class AlbumList extends Component {
     console.log(this.state);
 
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
